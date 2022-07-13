52ef3410fc2b422ea1ff78f0f7e3960e345e3d9e
johancberg <johancberg@outlook.com>
2022-07-13 15:31:45 +0200

    Added listeners funtion

--- src/Signup.elm
+++ src/Signup.elm
@@ -1,9 +1,10 @@
-module Signup exposing (User)
+module Signup exposing (main)
 
 import Css exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (..)
-import VirtualDom
+import Html.Styled.Events exposing (onInput, onClick)
+import Browser
 
 type alias User =
     { name : String
@@ -20,66 +21,31 @@ initialModel =
     , loggedIn = False
     }
 
-
-view : User -> Html msg
+view : User -> Html Msg
 view user =
     div []
         [ h1 [ css [ paddingLeft (cm 3) ] ] [ text "Sign up" ]
         , styledForm []
             [ div []
                 [ text "Name" 
-                , styledInput [ id "name", type_ "text" ] []
+                , styledInput [ id "name", type_ "text", onInput SaveName ] []
                 ]
             , div []
                 [ text "Email"
-                , styledInput [ id "email", type_ "email" ] []
+                , styledInput [ id "email", type_ "email", onInput SaveEmail ] []
                 ]
             , div []
                 [ text "Password"
-                , styledInput [ id "password", type_ "password" ] []
+                , styledInput [ id "password", type_ "password", onInput SavePassword ] []
                 ]
             , div []
-                [ styledButton [ type_ "submit" ]
+                [ styledButton [ type_ "submit", onClick Signup ]
                     [ text "Create my account" ]
                 ]
             ]
         ]
 
-{--
-formStyle : List (Attribute msg)
-formStyle =
-    [ style "border-radius" "5px"
-    , style "background-color" "#f2f2f2"
-    , style "padding" "20px"
-    , style "width" "300px"
-    ]
-
-
-inputStyle : List (Attribute msg)
-inputStyle =
-    [ style "display" "block"
-    , style "width" "260px"
-    , style "padding" "12px 20px"
-    , style "margin" "8px 0"
-    , style "border" "none"
-    , style "border-radius" "4px"
-    ]
-
-
-buttonStyle : List (Attribute msg)
-buttonStyle =
-    [ style "width" "300px"
-    , style "background-color" "#397cd5"
-    , style "color" "white"
-    , style "padding" "14px 20px"
-    , style "margin-top" "10px"
-    , style "border" "none"
-    , style "border-radius" "4px"
-    , style "font-size" "16px"
-    ]
---}
-
-styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
+styledForm : List (Attribute Msg) -> List (Html Msg) -> Html Msg
 styledForm =
     styled Html.Styled.form
         [ borderRadius (px 5)
@@ -88,8 +54,7 @@ styledForm =
         , Css.width (px 300)
         ]
 
-
-styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
+styledInput : List (Attribute Msg) -> List (Html Msg) -> Html Msg
 styledInput =
     styled Html.Styled.input
         [ display block
@@ -100,8 +65,7 @@ styledInput =
         , borderRadius (px 4)
         ]
 
-
-styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
+styledButton : List (Attribute Msg) -> List (Html Msg) -> Html Msg
 styledButton =
     styled Html.Styled.button
         [ Css.width (px 300)
@@ -114,6 +78,28 @@ styledButton =
         , fontSize (px 16)
         ]
 
-main : VirtualDom.Node msg
+type Msg
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
+
+update : Msg -> User -> User
+update message user =
+    case message of
+        SaveName name ->
+            { user | name = name }
+        SaveEmail email ->
+            { user | email = email }
+        SavePassword password ->
+            { user | password = password }
+        Signup ->
+            { user | loggedIn = True }
+
+main : Program () User Msg
 main =
-    toUnstyled <| view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = \model -> toUnstyled (view model)
+        , update = update
+        }
