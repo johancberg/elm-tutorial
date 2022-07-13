4ac785bdc8b984bed50663c1373eb254655eb3f7
johancberg <johancberg@outlook.com>
2022-07-13 14:24:59 +0200

    Implemented style attributes

--- src/Signup.elm
+++ src/Signup.elm
@@ -1,7 +1,7 @@
 module Signup exposing (User)
 
 import Html exposing (..)
-import Html.Attributes exposing (id, type_)
+import Html.Attributes exposing (id, type_, style)
 
 type alias User =
     { name : String
@@ -18,29 +18,62 @@ initialModel =
     , loggedIn = False
     }
 
+
 view : User -> Html msg
 view user =
     div []
-        [ h1 [] [ text "Sign up" ]
-        , Html.form []
+        [ h1 [ style "padding-left" "3cm" ] [ text "Sign up" ]
+        , Html.form formStyle
             [ div []
                 [ text "Name" 
-                , input [ id "name", type_ "text" ] []
+                , input ([ id "name", type_ "text" ] ++ inputStyle) []
                 ]
             , div []
                 [ text "Email"
-                , input [ id "email", type_ "email" ] []
+                , input ([ id "email", type_ "email" ] ++ inputStyle) []
                 ]
             , div []
                 [ text "Password"
-                , input [ id "password", type_ "password" ] []
+                , input ([ id "password", type_ "password" ] ++ inputStyle) []
                 ]
             , div []
-                [ button [ type_ "submit" ]
+                [ button ([ type_ "submit" ] ++ buttonStyle)
                     [ text "Create my account" ]
                 ]
             ]
         ]
+
+formStyle : List (Attribute msg)
+formStyle =
+    [ style "border-radius" "5px"
+    , style "background-color" "#f2f2f2"
+    , style "padding" "20px"
+    , style "width" "300px"
+    ]
+
+
+inputStyle : List (Attribute msg)
+inputStyle =
+    [ style "display" "block"
+    , style "width" "260px"
+    , style "padding" "12px 20px"
+    , style "margin" "8px 0"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    ]
+
+
+buttonStyle : List (Attribute msg)
+buttonStyle =
+    [ style "width" "300px"
+    , style "background-color" "#397cd5"
+    , style "color" "white"
+    , style "padding" "14px 20px"
+    , style "margin-top" "10px"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    , style "font-size" "16px"
+    ]
 
 main : Html msg
 main =
