5e026ad5dcbb518ca8bec6216391cf2887c76846
johancberg <johancberg@outlook.com>
2022-08-04 15:38:40 +0200

    Include functions in Post.elm

---
+++ post-app/Post.elm
@@ -0,0 +1,40 @@
+module Post exposing (Post, PostId, idToString, postDecoder, postsDecoder)
+
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Post =
+    { id : PostId
+    , title : String
+    , authorName : String
+    , authorUrl : String
+    }
+
+
+type PostId
+    = PostId Int
+
+
+postsDecoder : Decoder (List Post)
+postsDecoder =
+    list postDecoder
+
+
+idDecoder : Decoder PostId
+idDecoder =
+    Decode.map PostId int
+
+
+postDecoder : Decoder Post
+postDecoder =
+    Decode.succeed Post
+        |> required "id" idDecoder
+        |> required "title" string
+        |> required "authorName" string
+        |> required "authorUrl" string
+
+
+idToString : PostId -> String
+idToString (PostId id) =
+    String.fromInt id
