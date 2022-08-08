0f7cc74cc376ca473a7fd7c6208f07d85ed4a6cf
johancberg <johancberg@outlook.com>
2022-08-08 11:14:41 +0200

    Implement new funtions in Post

--- post-app/Post.elm
+++ post-app/Post.elm
@@ -1,7 +1,8 @@
-module Post exposing (Post, PostId, idParser, idToString, postDecoder, postsDecoder)
+module Post exposing (Post, PostId, idParser, idToString, postDecoder, postsDecoder, postEncoder)
 
 import Json.Decode as Decode exposing (Decoder, int, list, string)
 import Json.Decode.Pipeline exposing (required)
+import Json.Encode as Encode
 import Url.Parser exposing (Parser, custom)
 
 
@@ -46,3 +47,18 @@ idParser =
     custom "POSTID" <|
         \postId ->
             Maybe.map PostId (String.toInt postId)
+
+
+postEncoder : Post -> Encode.Value
+postEncoder post =
+    Encode.object
+        [ ( "id", encodeId post.id )
+        , ( "title", Encode.string post.title )
+        , ( "authorName", Encode.string post.authorName )
+        , ( "authorUrl", Encode.string post.authorUrl )
+        ]
+
+
+encodeId : PostId -> Encode.Value
+encodeId (PostId id) =
+    Encode.int id
