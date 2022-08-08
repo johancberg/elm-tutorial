db73eb1950901c805f105b689f50981d6083de9c
johancberg <johancberg@outlook.com>
2022-08-08 11:14:22 +0200

    Implement additional Route functions

--- post-app/Route.elm
+++ post-app/Route.elm
@@ -1,5 +1,6 @@
-module Route exposing (Route(..), parseUrl)
+module Route exposing (Route(..), parseUrl, pushUrl)
 
+import Browser.Navigation as Nav
 import Post exposing (PostId)
 import Url exposing (Url)
 import Url.Parser exposing (..)
@@ -28,3 +29,22 @@ matchRoute =
         , map Posts (s "posts")
         , map Post (s "posts" </> Post.idParser)
         ]
+
+
+pushUrl : Route -> Nav.Key -> Cmd msg
+pushUrl route navKey =
+    routeToString route
+        |> Nav.pushUrl navKey
+
+
+routeToString : Route -> String
+routeToString route =
+    case route of
+        NotFound ->
+            "/not-found"
+
+        Posts ->
+            "/posts"
+
+        Post postId ->
+            "/posts/" ++ Post.idToString postId
