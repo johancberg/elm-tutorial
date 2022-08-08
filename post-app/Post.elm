45477877ffff73ed2c258a4ad2be4c96bd8ce88e
johancberg <johancberg@outlook.com>
2022-08-08 14:25:27 +0200

    Add new emptyPost-functions

--- post-app/Post.elm
+++ post-app/Post.elm
@@ -1,4 +1,4 @@
-module Post exposing (Post, PostId, idParser, idToString, postDecoder, postsDecoder, postEncoder)
+module Post exposing (Post, PostId, emptyPost, idParser, idToString, newPostEncoder, postDecoder, postEncoder, postsDecoder)
 
 import Json.Decode as Decode exposing (Decoder, int, list, string)
 import Json.Decode.Pipeline exposing (required)
@@ -16,6 +16,20 @@ type alias Post =
 
 type PostId
     = PostId Int
+
+
+emptyPost : Post
+emptyPost =
+    { id = emptyPostId
+    , title = ""
+    , authorName = ""
+    , authorUrl = ""
+    }
+
+
+emptyPostId : PostId
+emptyPostId =
+    PostId -1
 
 
 postsDecoder : Decoder (List Post)
@@ -62,3 +76,12 @@ postEncoder post =
 encodeId : PostId -> Encode.Value
 encodeId (PostId id) =
     Encode.int id
+
+
+newPostEncoder : Post -> Encode.Value
+newPostEncoder post =
+    Encode.object
+        [ ( "title", Encode.string post.title )
+        , ( "authorName", Encode.string post.authorName )
+        , ( "authorUrl", Encode.string post.authorUrl )
+        ]
