c403776ca00ff1649f00488b3abee35723fc470e
johancberg <johancberg@outlook.com>
2022-08-23 16:31:25 +0200

    Implement the first MVC of PortExamples.

---
+++ src/PortExamples.elm
@@ -0,0 +1,44 @@
+module PortExamples exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    String
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick SendDataToJS ]
+            [ text "Send Data to JavaScript" ]
+        ]
+
+
+type Msg
+    = SendDataToJS
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SendDataToJS ->
+            ( model, Cmd.none )
+            
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( "", Cmd.none )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
