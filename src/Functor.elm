759fe56687b4994fafaaf52446e590a85d017d6b
johancberg <johancberg@outlook.com>
2022-08-31 11:11:16 +0200

    Change name of the Functor file

---
+++ src/Functor.elm
@@ -0,0 +1,48 @@
+module Functor exposing (..)
+import Html.Attributes exposing (list)
+import List exposing (head)
+
+type Tree node
+    = Node node (List (Tree node))
+
+test : Tree String
+test =
+    Node "Root"
+        [ Node "1 Level 1" []
+        , Node "2 Level 1" []
+        , Node "3 Level 1"
+            [ Node "1 Level 2" []
+            , Node "2 Level 2" []
+            , Node "4 Level 2" []
+            ]
+        ]
+
+-- Functor
+map : (a -> b) -> Tree a -> Tree b
+map func tree =
+    case tree of
+        Node str list ->
+            case List.head list of
+                Nothing -> Node (func str) []
+                Just edge  -> map func edge
+
+-- Monad
+--andThen : (a -> Tree b) -> Tree a -> Tree b
+
+--Traversable
+fold : (a -> b -> b) -> b -> Tree a -> b
+fold func node tree =
+    case tree of
+        Node str list ->
+            case (List.head list, List.tail list) of
+                (Nothing, Just tail) ->
+                    fold func node (Node str tail)
+                
+                (Just (Node head []), Nothing) ->
+                    fold func (func head node) (Node str [])
+                
+                (Just (Node head _), Just tail) ->
+                    fold func (func head node) (Node str tail)
+                
+                _ ->
+                    func str node
