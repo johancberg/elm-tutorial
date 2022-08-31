96fe554c9e733c474f1a322c1575869474b7fb33
johancberg <johancberg@outlook.com>
2022-08-31 14:36:03 +0200

    Get the tasks reviewed

--- src/Functor.elm
+++ src/Functor.elm
@@ -2,8 +2,8 @@ module Functor exposing (..)
 import Html.Attributes exposing (list)
 import List exposing (head)
 
-type Tree node
-    = Node node (List (Tree node))
+type Tree a
+    = Node a (List (Tree a))
 
 test : Tree String
 test =
@@ -17,32 +17,66 @@ test =
             ]
         ]
 
+type alias Maybes a = List (Maybe a)
+
+-- Maybes.map : (a -> b) -> (Maybes a -> Maybes b)
+-- Maybes.map func = List.map (Maybe.map func)
+--
+
+-- func : (a -> b)
+-- items : List (Maybe a)
+-- resultat : List (Maybe a) -> List (Maybe b)
+-- resultat = List.map (Maybe.map func)
+
+-- List.map : (a -> b) -> List a -> List b
+-- Maybe.map : (a -> b) -> Maybe a -> Maybe b
+-- fisk : (Maybe a) -> (Maybe b)
+-- fisk = Maybe.map func
+
+
 -- Functor
-map : (a -> b) -> Tree a -> Tree b
-map func tree =
-    case tree of
-        Node str list ->
-            case List.head list of
-                Nothing -> Node (func str) []
-                Just edge  -> map func edge
+map : (a -> b) -> (Tree a -> Tree b) -- === (a -> b) -> (Tree a -> Tree b)
+map func (Node str list) =
+    Node (func str) (List.map (map func) list)
+
+    -- str: a
+    -- list: List (Tree a)
+    -- func: (a -> b)
+
+    -- hej: List (Tree a) -> List (Tree b)
+    -- hej = List.map (map func)
+
+    -- Node (func str) (List.map (map func) list)
+
+-- List.map : ((Tree a) -> (Tree b)) -> List (Tree a) -> List (Tree b)
 
 -- Monad
 --andThen : (a -> Tree b) -> Tree a -> Tree b
 
 --Traversable
 fold : (a -> b -> b) -> b -> Tree a -> b
-fold func node tree =
-    case tree of
-        Node str list ->
-            case (List.head list, List.tail list) of
-                (Nothing, Just tail) ->
-                    fold func node (Node str tail)
-                
-                (Just (Node head []), Nothing) ->
-                    fold func (func head node) (Node str [])
-                
-                (Just (Node head _), Just tail) ->
-                    fold func (func head node) (Node str tail)
-                
-                _ ->
-                    func str node
+fold func node (Node str list) =
+    case (List.head list, List.tail list) of
+        (Just (Node head []), Nothing) ->
+            fold func (func head node) (Node str [])
+        
+        (Just (Node head child), Just tail) ->
+            fold func (func head node) (Node str (List.append child tail))
+        
+        _ ->
+            func str node
+-- Använd List.fold!
+
+
+{--
+toTuple : List a -> Maybe (a,a)
+toPair : List a -> List (a,a) 
+
+case list of
+    [] ->
+    [x] ->
+    head::heas2::tail -> 
+
+
+
+--}
