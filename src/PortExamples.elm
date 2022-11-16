a25259cac25aaa7c46837f232b1138bdaff0fa0b
johancberg <johancberg@outlook.com>
2022-11-16 23:04:59 +0100

    Expand PortExample to handle errors

--- src/PortExamples.elm
+++ src/PortExamples.elm
@@ -3,10 +3,24 @@ port module PortExamples exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Json.Decode exposing (Error(..), Value, decodeValue, string)
 
 
 type alias Model =
-    String
+    { dataFromJS : String
+    , jsonError : Maybe Error
+    }
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { dataFromJS = ""
+    , jsonError = Nothing
+    }
 
 
 view : Model -> Html Msg
@@ -14,15 +28,52 @@ view model =
     div []
         [ button [ onClick SendDataToJS ]
             [ text "Send Data to JavaScript" ]
-        , br [] []
-        , br [] []
-        , text ("Data received from JavaScript: " ++ model)
+        , viewDataFromJSOrError model
+        ]
+
+
+viewDataFromJSOrError : Model -> Html Msg
+viewDataFromJSOrError model =
+    case model.jsonError of
+        Just error ->
+            viewError error
+
+        Nothing ->
+            viewDataFromJS model.dataFromJS
+
+
+viewError : Error -> Html Msg
+viewError jsonError =
+    let
+        errorHeading =
+            "Couldn't receive data from JavaScript"
+
+        errorMessage =
+            case jsonError of
+                Failure message _ ->
+                    message
+
+                _ ->
+                    "Error: Invalid JSON"
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error: " ++ errorMessage)
+        ]
+
+
+viewDataFromJS : String -> Html msg
+viewDataFromJS data =
+    div []
+        [ br [] []
+        , strong [] [ text "Data received from JavaScript: " ]
+        , text data
         ]
 
 
 type Msg
     = SendDataToJS
-    | ReceivedDataFromJS Model
+    | ReceivedDataFromJS Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -30,21 +81,24 @@ update msg model =
     case msg of
         SendDataToJS ->
             ( model, sendData "Hello JavaScript!" )
-        ReceivedDataFromJS data ->
-            ( data, Cmd.none )
+            
+        ReceivedDataFromJS value ->
+            case decodeValue string value of
+                Ok data ->
+                    ( { model | dataFromJS = data }, Cmd.none )
+
+                Err error ->
+                    ( { model | jsonError = Just error }, Cmd.none )
             
 
 port sendData : String -> Cmd msg
-port receiveData : (Model -> msg) -> Sub msg
+port receiveData : (Value -> msg) -> Sub msg
 
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( "", Cmd.none )
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     receiveData ReceivedDataFromJS
+
 
 main : Program () Model Msg
 main =
